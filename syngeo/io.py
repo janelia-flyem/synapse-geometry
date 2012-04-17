22ba6eed1b2061bf3281a2185b12ea1a9bbdcccb
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-04-17 13:19:57 -0400

    Add get_box to obtain patch centered on coordinates

--- syngeo/io.py
+++ syngeo/io.py
@@ -3,7 +3,7 @@ import sys, os
 
 # external libraries
 import numpy as np
-from ray import imio, evaluate
+from ray import imio, evaluate, morpho
 
 def add_anything(a, b):
     return a + b
@@ -23,3 +23,16 @@ def write_synapse_to_vtk(neurons, coords, fn, im=None, t=(2,0,1), s=(1,-1,1),
     if im is not None:
         imio.write_vtk(im, 
             os.path.join(os.path.dirname(fn), 'image.' + os.path.basename(fn)))
+
+def get_box(a, coords, margin):
+    """Obtain a box of size 2*margin+1 around coords in array a.
+
+    Boxes close to the boundary are trimmed accordingly.
+    """
+    coords = np.array(coords)[np.newaxis, :]
+    origin = np.zeros(coords.shape, dtype=int)
+    shape = np.array(a.shape)[np.newaxis, :]
+    topleft = np.concatenate((coords-margin, origin), axis=0).max(axis=0)
+    bottomright = np.concatenate((coords+margin+1, shape), axis=0).min(axis=0)
+    box = [slice(top, bottom) for top, bottom in zip(topleft, bottomright)]
+    return a[box].copy()
