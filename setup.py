458cab36ab40b21e05d1be2fc6e86b777a0760c1
Bill Katz <katzw@janelia.hhmi.org>
2012-11-08 16:53:10 -0500

    Modify license slightly and add setup.py for install into standard python lib

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "Synapse-geometry",
+    version = "1.0",
+    description = "Synapse-geometry is a python library to work with neuroimaging data at synapse-level resolution, distributed under the open-source BSD license",
+    author = "Juan Nunez-Iglesias",
+    url = "https://github.com/jni/synapse-geometry",
+    author_email = 'jni@janelia.hhmi.org',
+    packages = ['syngeo']
+)
